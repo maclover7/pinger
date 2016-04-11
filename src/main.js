893e49ab5d668363fc3a5d77fed01d04e8030363
Jon Moss <me@jonathanmoss.me>
2016-04-11 14:51:17 -0400

    Remove ES6 code -- CommonJS all the way!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App'
+var Vue = require('vue');
+var App = require('./App');
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
-})
+  components: {
+    App
+  }
+});
